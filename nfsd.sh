5272e5b5d62bb74dde82b0c9bb677f5d97afbfc0
Scott Merchant <scott.merchant@octopus.com>
2024-03-13 13:13:15 +1030

    add grace time 0

--- nfsd.sh
+++ nfsd.sh
@@ -108,7 +108,7 @@ while true; do
     # /usr/sbin/rpc.statd
 
     echo "Starting NFS in the background..."
-    /usr/sbin/rpc.nfsd --debug 8 --no-udp --no-nfs-version 3
+    /usr/sbin/rpc.nfsd --debug --grace-time 0 --no-udp --no-nfs-version 3
     echo "Exporting File System..."
     if /usr/sbin/exportfs -rv; then
       /usr/sbin/exportfs
